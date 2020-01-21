038a1d962f9eaaa452c14a3ca65fed120fbe3a53
yjb94 <jbyun94@naver.com>
2020-01-21 20:12:39 +0900

    feat: added image linking due locale

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+function ReactStoreBadges({
+  locale = (navigator.language || navigator.userLanguage || 'en-us').toLowerCase(),
+  shortLocale = locale.split(/[_-]/)[0],
+  imageLink = {
+    ios:`https://linkmaker.itunes.apple.com/images/badges/${locale}/badge_appstore-lrg.svg`,
+    // android: `https://play.google.com/intl/en_us/badges/static/images/badges/${shortLocale}_badge_web_generic.png`,
+    android: `https://rawgithub.com/steverichey/google-play-badge-svg/master/img/${shortLocale}_get.svg`,
+  },
+}) {
+
+  return (
+    <div>
+      <h2>{locale}</h2>
+      <img src={imageLink.ios}/>
+      <img src={imageLink.android}/>
     </div>
-  }
+  );
 }
+
+export default ReactStoreBadges
