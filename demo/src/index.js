c269ff41eb2f40549f95a96d07d56ff94a516b25
yjb94 <jbyun94@naver.com>
2020-01-22 17:45:20 +0900

    docs: demo updated

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,17 +5,49 @@ import ReactStoreBadges from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <ReactStoreBadges
-        platform={'ios'}
-        url={'https://apps.apple.com/app/kpop-speed-quiz/id1463167156'}
-      />
+    return (
+      <div 
+        style={{
+          fontFamily:'Arial',
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center'
+        }}
+      >
+        <h1>React Store Badges</h1>
+        <div
+          style={{
+            width:'600px',
+            display:'flex',
+            justifyContent:'space-between'
+          }}
+        >
+          <ReactStoreBadges
+            platform={'ios'}
+            url={'https://apps.apple.com/app/'}
+            locale={'en-us'}
+          />
 
-      <ReactStoreBadges
-        platform={'android'}
-        url={'https://play.google.com/store/apps/details?id=com.stayge.speedquiz'}
-      />
-    </div>
+          <ReactStoreBadges
+            platform={'android'}
+            url={'https://play.google.com/store/apps/details?id='}
+            locale={'ko-kr'}
+          />
+
+          <ReactStoreBadges
+            platform={'ios'}
+            url={'https://apps.apple.com/app/'}
+            locale={'pt-pt'}
+          />
+
+          <ReactStoreBadges
+            platform={'android'}
+            url={'https://play.google.com/store/apps/details?id='}
+            locale={'zh-cn'}
+          />
+        </div>
+      </div>
+    )
   }
 }
 
