18d422b96742ba06aed62cbb3b0308ed32024f1b
yjb94 <jbyun94@naver.com>
2020-01-23 13:35:15 +0900

    docs: updated demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,46 +4,81 @@ import {render} from 'react-dom'
 import ReactStoreBadges from '../../src'
 
 class Demo extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      locale:'en-us'
+    };
+  }
+
+  changeLocale = () => {
+    const { locale } = this.state;
+    this.setState({ locale:locale === 'en-us' ? 'ko-kr' : 'en-us' })
+  }
+
   render() {
+    const { locale } = this.state;
+
     return (
       <div 
         style={{
           fontFamily:'Arial',
+          textAlign:'center',
           display:'flex',
           flexDirection:'column',
           alignItems:'center'
         }}
       >
-        <h1>React Store Badges</h1>
         <div
           style={{
-            width:'600px',
-            display:'flex',
-            justifyContent:'space-between'
+            marginBottom:'60px'
           }}
         >
-          <ReactStoreBadges
-            platform={'ios'}
-            url={'https://apps.apple.com/app/'}
-          />
+          <h1>React Store Badges</h1>
+          <div
+            style={{
+              width:'600px',
+              display:'flex',
+              justifyContent:'space-between'
+            }}
+          >
+            <ReactStoreBadges
+              platform={'ios'}
+              url={'https://apps.apple.com/app/'}
+            />
+            <ReactStoreBadges
+              platform={'android'}
+              url={'https://play.google.com/store/apps/details?id='}
+              locale={'ko-kr'}
+            />
+            <ReactStoreBadges
+              platform={'ios'}
+              url={'https://apps.apple.com/app/'}
+              locale={'es-ES'}
+            />
 
-          <ReactStoreBadges
-            platform={'android'}
-            url={'https://play.google.com/store/apps/details?id='}
-            locale={'ko-kr'}
-          />
-
-          <ReactStoreBadges
-            platform={'ios'}
-            url={'https://apps.apple.com/app/'}
-            locale={'pt-pt'}
-          />
-
-          <ReactStoreBadges
-            platform={'android'}
-            url={'https://play.google.com/store/apps/details?id='}
-            locale={'zh-cn'}
-          />
+            <ReactStoreBadges
+              platform={'android'}
+              url={'https://play.google.com/store/apps/details?id='}
+              locale={'zh-CN'}
+            />
+          </div>
+        </div>
+        <div>
+          <h1>Locale Change</h1>
+          <div>
+            <ReactStoreBadges
+              platform={'ios'}
+              url={'https://apps.apple.com/app/'}
+              locale={locale}
+            />
+          </div>
+          <button
+            onClick={this.changeLocale}
+          >
+            change language
+          </button>
         </div>
       </div>
     )
