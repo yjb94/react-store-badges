8a1e04bba48df2c0dfac70feb688c2c3f346594a
Arnaud Cortisse <cortissearnaud@gmail.com>
2020-07-03 09:26:23 +0200

    new: can add 'alt' property to <img />

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const HEIGHT_RATIO = 3.375;
 
-const getImage = (locale = 'en-us', code = locale) => {
+const getImage = (locale = "en-us", code = locale) => {
   return {
-    ios:`https://linkmaker.itunes.apple.com/images/badges/${locale}/badge_appstore-lrg.svg`,
-    android: `https://raw.github.com/yjb94/google-play-badge-svg/master/img/${code}_get.svg?sanitize=true`
-  }
-}
+    ios: `https://linkmaker.itunes.apple.com/images/badges/${locale}/badge_appstore-lrg.svg`,
+    android: `https://raw.github.com/yjb94/google-play-badge-svg/master/img/${code}_get.svg?sanitize=true`,
+  };
+};
 
 const ReactStoreBadges = ({
   url,
-  defaultLocale = 'en-us',
+  defaultLocale = "en-us",
   platform,
-  locale = (typeof navigator !== 'undefined' && navigator.language || defaultLocale),
+  locale = (typeof navigator !== "undefined" && navigator.language) ||
+    defaultLocale,
   width = 135,
-  height = width/HEIGHT_RATIO,
-  target = "_self"
+  height = width / HEIGHT_RATIO,
+  target = "_self",
+  alt = "store badge",
 }) => {
-  let shortCode = locale = locale.toLowerCase()
+  let shortCode = (locale = locale.toLowerCase());
   const expeptionLocale = ["zh-cn", "zh-tw"];
   if (expeptionLocale.indexOf(locale) === -1) {
     shortCode = locale.split(/[_-]/)[0];
   }
 
-  const [image, setImage] = useState(getImage(locale, shortCode))
+  const [image, setImage] = useState(getImage(locale, shortCode));
 
   const setDefaultImage = () => {
-    setImage(getImage(defaultLocale, shortCode))
-  }
+    setImage(getImage(defaultLocale, shortCode));
+  };
 
   return (
     <a
       style={{
-        display: 'inline-block',
+        display: "inline-block",
         height: height,
         width: width,
       }}
       href={url}
       target={target}
     >
-      <img 
+      <img
         src={image[platform]}
         style={{
-          width:'100%',
-          height:'100%'
+          width: "100%",
+          height: "100%",
         }}
         onError={setDefaultImage}
+        alt={alt}
       />
     </a>
   );
-}
-export default ReactStoreBadges
+};
+export default ReactStoreBadges;
